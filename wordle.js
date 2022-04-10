300c5a30d76c50257939ca7fdb69fb113bef3823
Denis Kartachov <kartachovd@gmail.com>
2022-04-10 19:12:03 -0400

    add delete and submit

--- wordle.js
+++ wordle.js
@@ -1,37 +1,84 @@
-const data = {
-    r0: ['', '', '', '', ''],
-    r1: ['', '', '', '', ''],
-    r2: ['', '', '', '', ''],
-    r3: ['', '', '', '', ''],
-    r4: ['', '', '', '', '']
-};
-
-const validKeys = ['']
-
-let nextId = 'r0c0';
+const answer = 'crate';
+let nextId = '00';
+let guess = '';
+let guesses = 0;
+let gameOver = false;
 
 document.addEventListener('keydown', (event) => {
+    if (gameOver) return;
+
+    const cell = document.getElementById(nextId);
     const key = event.key;
+    
+    if (key === 'Enter') {
+        if (guess.length < 5) return;
+
+        nextId = getNextRow(nextId);
+        guess = '';
+        guesses++;
+
+        if (guesses === 5) {
+            gameOver = true;
+
+            alert('Game over');
+            
+            return;
+        }
+    }
+
+    if (key === 'Backspace') {
+        if (cell.textContent) {
+            cell.textContent = '';
+        } else {
+            nextId = getPrevCol(nextId);
+            document.getElementById(nextId).textContent = '';
+        }
+
+        guess = guess.slice(0, -1);
+        return;
+    }
 
     if (!isLetter(key)) return;
 
-    document.getElementById(nextId).textContent = key;
+    guess += guess.length < 5 ? key: '';
+    cell.textContent = key.toUpperCase();
+    nextId = getNextCol(nextId);
 
-    nextId = getNextId(nextId);
+    console.log(guess);
 });
 
 function isLetter(str) {
     return str.length === 1 && str.match(/[a-z]/i);
 }
 
-function getNextId(id) {
-    let colId = id.substring(2);
-    let colNum = parseInt(colId.charAt(1));
+function getPrevCol(id) {
+    let row = parseInt(id.charAt(0));
+    let col = parseInt(id.charAt(1));
 
-    if (colNum < 4) {
-        colNum++;
-        return id.replace(colId, `c${colNum}`);
+    if (col > 0) {
+        return row + '' + (col - 1);
     }
 
+    return id;
+}
+
+function getNextCol(id) {
+    let row = parseInt(id.charAt(0));
+    let col = parseInt(id.charAt(1));
+
+    if (col < 4) {
+        return row + '' + (col + 1);
+    }
+
+    return id;
+}
+
+function getNextRow(id) {
+    let row = parseInt(id.charAt(0));
+
+    if(row < 5) {
+        return (row + 1) + '0';
+    }
+    
     return id;
 }
